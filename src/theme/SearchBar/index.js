30fcc49d5d42f4bdd2ede494e7e37cd5a0a12cc9
Anton Dosta <a.dosta@icloud.com>
2024-02-26 16:54:39 -0700

    update

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SearchBar from "@theme-original/SearchBar";
-import ChefGPT from "@cookbookdev/docusaurus-chefgpt/theme/SearchBar";
 
 export default function SearchBarWrapper(props) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://docsbot-standalone-git-develop-cookbookdev.vercel.app/docsbot.min.js";
+    script.defer = true;
+    document.head.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <>
       <SearchBar {...props} />
-      <ChefGPT />
+      <div id="__cookbook" data-api-key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWMxNzQ5ZDdlODQ5N2EyNWQ2NGJiMDYiLCJpYXQiOjE3MDcxNzcxMTcsImV4cCI6MjAyMjc1MzExN30.1aLVJhVnK5JfFXFmdcbQqJP_onJ94U2EL1xn-Tw0G8k" />
     </>
   );
 }
